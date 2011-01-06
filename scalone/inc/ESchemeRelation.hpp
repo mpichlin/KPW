c0a192ec821a5e5732f16cde9713e4c78189cf95
Michal Pichlinski <michal.pichlinski@gmail.com>
2011-01-06 17:17:50 +0100

    Dodano resztę dokumentacji API

--- scalone/inc/ESchemeRelation.hpp
+++ scalone/inc/ESchemeRelation.hpp
@@ -1,6 +1,21 @@
 #ifndef E_SCHEME_RELATION_HPP
 #define E_SCHEME_RELATION_HPP
 
+/*! \file ESchemeRelation.hpp
+ *  \brief Zawiera definicję typu wyliczeniowego ESchemeRelation
+ *
+ * Plik zawiera definicję typu wyliczeniowego ESchemeRelation
+ *
+ * \author Michał Pichliński
+ * \date 2010.12.11
+ * \version 1.00.00
+ */
+
+/*! \brief Typy relacji koncept - schemat
+ *
+ * Typ wyliczeniowy określający typy relacji koncept - schemat. Możliwe typy to:
+ * najwyższy w schemacie, w schemacie
+ */
 enum ESchemeRelation
 {
   Top      = 0,
